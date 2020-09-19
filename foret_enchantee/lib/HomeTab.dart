1cc1e3fb175f1cdbe226a162c732185708dadfa9
Andréa Beyer <48089121+AndreaBeyer@users.noreply.github.com>
2020-09-19 18:43:32 +0200

    --

--- foret_enchantee/lib/HomeTab.dart
+++ foret_enchantee/lib/HomeTab.dart
@@ -26,89 +26,103 @@ class _HomeTabState extends State<HomeTabState>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: hauteur - 75,
-        width: largeur,
-        decoration: new BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.5),
-                spreadRadius: 2,
-                blurRadius: 3,
-                offset: Offset(0, 1),
-              )
-            ]
-        ),
-        child: SingleChildScrollView(
-          child: Column(
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                margin: EdgeInsets.only(top: 15, left: 5, right: 5, bottom: 8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                height: hauteur - 75,
+                width: largeur,
+                decoration: new BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(0, 1),
+                      )
+                    ]
+                ),
+                padding: const EdgeInsets.all(3.0),
+                child: Column(
                   children: [
-                    Image.asset(
-                      "asset/Altkirch.png",
-                      height: 50,
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                        "Bienvenue à la forêt enchantée",
-                        style: new TextStyle(
-                          color: Colors.black,
-                          fontSize: 24,
-                          fontFamily: "Lobster",
-                        ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 15, left: 5, right: 5, bottom: 8),
-                child: Image.asset(
-                  "asset/enchantee.jpg",
-                  width: largeur,
-                  height: hauteur/3,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: hauteur / 3,
-                    width: largeur - 10,
-                    padding: const EdgeInsets.all(3.0),
-                    child: Wrap(
-                      direction: Axis.horizontal,
-                      children: [
-                        Text(
-                          "La Forêt Enchantée"
-                              " d’Altkirch illustre chaque année une dizaine de contes et légendes. \n\nAnimaux de la forêt, personnages mystérieux et décors magiques s’installent au centre-ville "
-                              "pour vous transporter vers un monde fantastique.\n\nLa période de Noël se veut particulière à Altkirch, pas de marché de Noël traditionnel mais une "
-                              "mise en scène originale transformant tout le centre-ville en une forêt magique.\n\nÀ cette occasion, découvrez de nombreuses animations :\n"
-                              " spectacles de rue et déambulations, animations à la patinoire en plein air, Village des artisans, etc…",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontFamily: "Lobster",
-                            fontSize: 15,
-                            color: secondColor,
+                    Container(
+                      margin: EdgeInsets.only(top: 5, left: 5, right: 5, bottom: 3),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            "asset/Altkirch.png",
+                            height: 50,
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: largeur,
+                      height: hauteur-150,
+                      child: Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              Container(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "Bienvenue à la forêt enchantée",
+                                      style: new TextStyle(
+                                        color: secondColor,
+                                        fontSize: 24,
+                                        fontFamily: "Lobster",
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(top: 15, left: 5, right: 5, bottom: 8),
+                                child: Image.asset(
+                                  "asset/enchantee.jpg",
+                                  width: largeur,
+                                  height: hauteur/4,
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(top: 15, left: 5, right: 5, bottom: 8),
+                                child: Wrap(
+                                  direction: Axis.horizontal,
+                                  children: [
+                                    Text(
+                                      "La Forêt Enchantée"
+                                          " d’Altkirch illustre chaque année une dizaine de contes et légendes. \n\nAnimaux de la forêt, personnages mystérieux et décors magiques s’installent au centre-ville "
+                                          "pour vous transporter vers un monde fantastique.\n\nLa période de Noël se veut particulière à Altkirch, pas de marché de Noël traditionnel mais une "
+                                          "mise en scène originale transformant tout le centre-ville en une forêt magique.\n\nÀ cette occasion, découvrez de nombreuses animations :\n"
+                                          " spectacles de rue et déambulations, animations à la patinoire en plein air, Village des artisans, etc…\n\n",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontFamily: "Lobster",
+                                        fontSize: 16,
+                                        color: secondColor,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
+                      ),
                     ),
-                  ),
-                ],
-              )
+                  ],
+                ),
+              ),
             ],
-          ),
-        ),
+          )
+        ],
       ),
     );
   }
